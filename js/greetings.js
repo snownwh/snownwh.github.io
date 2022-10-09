0a088d3776b833565db095868906ff77376f0e9d
WooHyun Nam <woohyunnam@WooHyunui-MacBookPro.local>
2022-10-10 03:24:23 +0900

    최종 20221010

--- js/greetings.js
+++ js/greetings.js
@@ -2,6 +2,7 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting span");
 const logoutForm = document.querySelector("#logout-form");
+const todo = document.getElementById("todo");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -11,18 +12,20 @@ function onLoginSubmit(event) {
   const usernameThatTheUserWrote = loginInput.value;
   localStorage.setItem(USERNAME_KEY, usernameThatTheUserWrote);
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  todo.classList.remove(HIDDEN_CLASSNAME);
 
   paintGreetings(usernameThatTheUserWrote);
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `Hello, ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
   logoutForm.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function onLogoutSubmit(event) {
   event.preventDefault();
+  todo.classList.add(HIDDEN_CLASSNAME);
   localStorage.removeItem(USERNAME_KEY);
   window.location.reload();
 }
@@ -34,8 +37,10 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
   // show the form
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  todo.classList.add(HIDDEN_CLASSNAME);
 } else {
   // show the greetings
+  todo.classList.remove(HIDDEN_CLASSNAME);
   paintGreetings(savedUsername);
 }
 
